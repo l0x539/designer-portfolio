3fc9a8d6245e748a66fdd2ff29d5cd16054acf44
l0x539 <nogues322@gmail.com>
2021-08-07 01:08:56 +0200

    Instructions.

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users (
+    username VARCHAR(100),
+    hash VARCHAR(100),
+    last_login DATETIME,
+    created_at DATETIME,
+);
+
+CREATE TABLE videos (
+    id int NOT NULL AUTO_INCREMENT,
+    url VARCHAR(1000),
+    title VARCHAR(100),
+    id PRIMARY KEY (id)
+);
