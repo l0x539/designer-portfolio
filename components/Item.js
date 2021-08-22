36c81c50c5e4cb6de25b390bfcfab8d370163ec7
l0x539 <nogues322@gmail.com>
2021-08-22 14:37:44 +0200

    Reset video seek onMouse leave (ref Hook).

--- components/Item.js
+++ components/Item.js
@@ -1,20 +1,25 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 
 const Item = ({url, isFirst}) => {
     const [playing, setPlaying] = useState(false)
-
+    const seeker = useRef();
     const startPlaying = () => {
         setPlaying(true)
+
     }
 
     const pausePlaying = () => {
         setPlaying(false)
     }
 
+    const resetSeek = () => {
+        seeker.current.seekTo(0)
+    }
+
     return (
         <div className={`container__video ${isFirst?'container__first':'container__video--lised'}`} onMouseEnter={startPlaying} onMouseLeave={pausePlaying}>
-            <ReactPlayer url={url} loop playing={playing||isFirst} muted={true} style={{width:"1px", height: "1px"}} width={"100%"} height={isFirst?"unset":"100%"} />
+            <ReactPlayer ref={seeker} onPause={resetSeek} url={url} loop playing={playing||isFirst} muted={true} style={{width:"1px", height: "1px"}} width={"100%"} height={isFirst?"unset":"100%"} />
         </div>
     )
 }
