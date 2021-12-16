bf58fc77a6104c372844f66e02c1cfb557e0b9c7
l0x539 <nogues322@gmail.com>
2021-12-16 13:05:57 +0100

    Loop videos.

--- components/Item.js
+++ components/Item.js
@@ -3,7 +3,6 @@ import ReactPlayer from 'react-player';
 
 const Item = ({url, isFirst}) => {
     const [playing, setPlaying] = useState(false)
-    const seeker = useRef();
     const startPlaying = () => {
         setPlaying(true)
 
@@ -13,13 +12,9 @@ const Item = ({url, isFirst}) => {
         setPlaying(false)
     }
 
-    const resetSeek = () => {
-        seeker.current.seekTo(0)
-    }
-
     return (
         <div className={`container__video ${isFirst?'container__first':'container__video--lised'}`} onMouseEnter={startPlaying} onMouseLeave={pausePlaying}>
-            <ReactPlayer ref={seeker} onPause={resetSeek} url={url} loop playing={playing||isFirst} muted={true} style={{width:"1px", height: "1px"}} width={"100%"} height={isFirst?"unset":"100%"} />
+            <ReactPlayer url={url} loop playing={playing||isFirst} muted={true} style={{width:"1px", height: "1px"}} width={"100%"} height={isFirst?"unset":"100%"} />
         </div>
     )
 }
